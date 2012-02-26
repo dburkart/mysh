1cb79c1f7539cb6579fa88f0cbefb074b71f169b
Gideon Williams <gaw5951@gmail.com>
2012-02-26 17:30:34 -0500

    Pipe partially works, grep still wont stop waiting for input

--- interpret.c
+++ interpret.c
@@ -20,210 +20,206 @@ size_t stack_size = 0;
 void printcmd( cmd, char * );
 
 void interpret( cmd_list cmds ) {
-	reverse_polish( cmds );
-	cmd a, b, c;
-	char buff[BUFF_SIZE];
-	pid_t result;
-	FILE *fp, *fd;
-	int next;
+    reverse_polish( cmds );
+    cmd a, b, c;
+    char buff[BUFF_SIZE];
+    pid_t result;
+    FILE *fp, *fd;
+    int next;
 
-	char *pipestr[1], *lessstr[1], *greastr[1];
-	pipestr[0] = "PIPE-OUTPUT";
-	lessstr[0] = "READ-OUTPUT";
-	greastr[0] = "GREA-OUTPUT";
+    char *pipestr[1], *lessstr[1], *greastr[1];
+    pipestr[0] = "PIPE-OUTPUT";
+    lessstr[0] = "READ-OUTPUT";
+    greastr[0] = "GREA-OUTPUT";
 
-	c = cmdlist_pop();
-	do {
+    c = cmdlist_pop();
+    do {
 
-		if (cmdlist_len(stack) == 0 && c.pmode) {
-			waitpid( result, NULL, NULL );
-		} else {
+        if (cmdlist_len(stack) == 0 && c.pmode) {
+            waitpid( result, NULL, 0 );
+        } else {
 
-		switch (c.pmode) {
-			case C_PIPE:
-				a = cmdlist_pop();
-				b = cmdlist_pop();
-				
-				printcmd( c, "pipe(c)" );
-				printcmd( a, "     a" );
-				printcmd( b, "     b" );
-				
-				result = fork();
+            switch (c.pmode) {
+                case C_PIPE:
+                    a = cmdlist_pop();
+                    b = cmdlist_pop();
 
-				if (result == 0) {
-					if ( a.pmode ) {
-						close( 0 );
-						dup( a.fd_out[0] );
-						
-						if ( c.fd_out[1] != 1 ) {
-							close( 1 );
-							dup( c.fd_out[1] );
-						}
-						
-						execvp( b.list[0], b.list );
-						cmdlist_push( c );
-					} else {
-						pipe( b.fd_in );
-						close( 1 );
-						dup( b.fd_in[1] );
-						close( b.fd_in[0] );
-						
-						execvp( a.list[0], a.list );
-						cmdlist_push( b );
-					}
-				
-/*					close( 0 );*/
-/*					freopen(b.list[0], "r", stdin);*/
+                    printcmd( c, "pipe(c)" );
+                    printcmd( a, "     a" );
+                    printcmd( b, "     b" );
+                        if ( a.pmode ) {
 
-/*					if ( c.fd_out[1] != 1 ) {*/
-/*						close( 1 );*/
-/*						dup( c.fd_out[1] );*/
-/*					}*/
-				} else if (result == -1) {
+                            cmdlist_push( c );
+                        } else {
+                            pipe( b.fd_in );
+                            cmdlist_push( b );
+                        }
 
-				}
-				
-				break;
+                    result = fork();
 
-			case C_LESS:
-				a = cmdlist_pop();
-				b = cmdlist_pop();
+                    if (result == 0) {
+                        if ( a.pmode ) {
+                            close( 0 );
+                            dup( a.fd_out[0] );
 
-				printcmd( c, "less(c)" );
-				printcmd( a, "     a" );
-				printcmd( b, "     b" );
+                            if ( c.fd_out[1] != 1 ) {
+                                close( 1 );
+                                dup( c.fd_out[1] );
+                            }
 
-				result = fork();
+                            execvp( b.list[0], b.list );
+                        } else {
+                            close( 1 );
+                            dup( b.fd_in[1] );
+                            close( b.fd_in[0] );
 
-				if (result == 0) {
-					close( 0 );
-					freopen(b.list[0], "r", stdin);
+                            execvp( a.list[0], a.list );
+                        }
+                    } else if (result == -1) {
 
-					if ( c.fd_out[1] != 1 ) {
-						close( 1 );
-						dup( c.fd_out[1] );
-					}
+                    }
 
-					execvp( a.list[0], a.list );
-					
-				} else if (result == -1) {
+                    break;
 
-				}
-				
-				cmdlist_push( c );
-				
-				break;
+                case C_LESS:
+                    a = cmdlist_pop();
+                    b = cmdlist_pop();
 
-			case C_GREA:
-				a = cmdlist_pop();
-				b = cmdlist_pop();
+                    printcmd( c, "less(c)" );
+                    printcmd( a, "     a" );
+                    printcmd( b, "     b" );
 
-				printf( "WRITING '%s*' INTO '%s*'\n", a.list[0], b.list[0] );
-				break;
+                    result = fork();
 
-			default:
-				printcmd( c, "execute" );
-			
-				result = fork();
+                    if (result == 0) {
+                        close( 0 );
+                        freopen(b.list[0], "r", stdin);
 
-				if (result == 0) {
-					if (c.fd_in[0] != 0) {
-						close( 0 );
-						dup( c.fd_in[0] );
-						close( c.fd_in[1] );
-					}
-					
-					if (c.fd_out[1] != 1) {
-						close( 1 );
-						dup( c.fd_out[1] );
-						close( c.fd_out[0] );
-					}
-				
-					execvp( c.list[0], c.list );
-				} else if (result == -1) {
-					perror( "Fork blew up\n" );
-				} else {
-					waitpid( result, NULL, NULL );
-				}
+                        if ( c.fd_out[1] != 1 ) {
+                            close( 1 );
+                            dup( c.fd_out[1] );
+                        }
 
-				break;
-		}
-		}
+                        execvp( a.list[0], a.list );
 
-		stack_swap();
-		c = cmdlist_pop();
-	} while (c.list != NULL || c.pmode != NULL);
+                    } else if (result == -1) {
 
-	stack_size = 0;
-	free( stack );
-	stack = NULL;
+                    }
+
+                    cmdlist_push( c );
+
+                    break;
+
+                case C_GREA:
+                    a = cmdlist_pop();
+                    b = cmdlist_pop();
+
+                    printf( "WRITING '%s*' INTO '%s*'\n", a.list[0], b.list[0] );
+                    break;
+
+                default:
+                    printcmd( c, "execute" );
+
+                    result = fork();
+
+                    if (result == 0) {
+                        if (c.fd_in[0] != 0) {
+                            close( 0 );
+                            dup( c.fd_in[0] );
+                            close( c.fd_in[1] );
+                        }
+
+                        if (c.fd_out[1] != 1) {
+                            close( 1 );
+                            dup( c.fd_out[1] );
+                            close( c.fd_out[0] );
+                        }
+
+                        execvp( c.list[0], c.list );
+                    } else if (result == -1) {
+                        perror( "Fork blew up\n" );
+                    } else {
+                        waitpid( result, NULL, 0 );
+                    }
+
+                    break;
+            }
+        }
+
+        stack_swap();
+        c = cmdlist_pop();
+    } while (c.list != NULL || c.pmode != 0 );
+
+    stack_size = 0;
+    free( stack );
+    stack = NULL;
 
 }
 
 cmd_list reverse_polish( cmd_list cmds ) {
-	size_t len = cmdlist_len( cmds );
-	stack = NULL;
-	cmd prev = (cmd){0, 0, {0, 1}, {-1, -1}};
+    size_t len = cmdlist_len( cmds );
+    stack = NULL;
+    cmd prev = (cmd){0, 0, {0, 1}, {-1, -1}};
 
-	// Reverse the command list
-	for ( int i = len-1; i >= 0; i-- ) {
-		if ( cmds[i].pmode ) {
-			if ( prev.pmode ) {
-				pipe( cmds[i].fd_out );
-				prev.fd_in[0] = cmds[i].fd_out[0];
-				prev.fd_in[1] = cmds[i].fd_out[1];
-			}
-			prev = cmds[i];
-		}
-	
-		cmdlist_push( cmds[i] );
-	}
+    // Reverse the command list
+    for ( int i = len-1; i >= 0; i-- ) {
+        if ( cmds[i].pmode ) {
+            if ( prev.pmode ) {
+                pipe( cmds[i].fd_out );
+                prev.fd_in[0] = cmds[i].fd_out[0];
+                prev.fd_in[1] = cmds[i].fd_out[1];
+            }
+            prev = cmds[i];
+        }
 
-	stack_swap();
+        cmdlist_push( cmds[i] );
+    }
 
-	return stack;
+    stack_swap();
+
+    return stack;
 }
 
 cmd_list cmdlist_push( cmd c ) {
-	size_t len = cmdlist_len( stack );
+    size_t len = cmdlist_len( stack );
 
-	if ( len < stack_size ) {
-		stack[ len ] = c;
-	} else {
-		stack = cmdlist_add( stack, c );
-		stack_size++;
-	}
+    if ( len < stack_size ) {
+        stack[ len ] = c;
+    } else {
+        stack = cmdlist_add( stack, c );
+        stack_size++;
+    }
 
-	return stack;
+    return stack;
 }
 
 void stack_swap() {
-	size_t len = cmdlist_len( stack );
+    size_t len = cmdlist_len( stack );
 
-	if ( len <= 2 ) return;
+    if ( len <= 2 ) return;
 
-	cmd c = stack[len-1];
+    cmd c = stack[len-1];
 
-	stack[len-1] = stack[len-2];
-	stack[len-2] = c;
+    stack[len-1] = stack[len-2];
+    stack[len-2] = c;
 }
 
 cmd cmdlist_pop() {
-	size_t len, new_size;
-	cmd c;
+    size_t len, new_size;
+    cmd c;
 
-	len = cmdlist_len( stack );
-	if ( !len ) {
-		return (cmd){0, 0, {-1, -1}};
-	}
+    len = cmdlist_len( stack );
+    if ( !len ) {
+        return (cmd){0, 0, {-1, -1}};
+    }
 
-	c = stack[ len - 1 ];
-	stack[ len - 1] = (cmd){0, 0, {-1, -1}};
+    c = stack[ len - 1 ];
+    stack[ len - 1] = (cmd){0, 0, {-1, -1}};
 
-	return c;
+    return c;
 }
 
 void printcmd( cmd c, char *where) {
-	printf( "%s: pmode: %d, fd_in: {%d, %d}, fd_out: {%d, %d}\n", where, c.pmode,
-			c.fd_in[0], c.fd_in[1], c.fd_out[0], c.fd_out[1] );
+    printf( "%s: pmode: %d, fd_in: {%d, %d}, fd_out: {%d, %d}\n", where, c.pmode,
+            c.fd_in[0], c.fd_in[1], c.fd_out[0], c.fd_out[1] );
 }
